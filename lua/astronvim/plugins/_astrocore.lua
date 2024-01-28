9c6294f4f2adc31952de40fbec8ec2381c3a05fc
Micah Halter <micah@mehalter.com>
2024-01-28 17:20:27 -0500

    fix(astrocore): prefer version control over project files

--- lua/astronvim/plugins/_astrocore.lua
+++ lua/astronvim/plugins/_astrocore.lua
@@ -20,7 +20,7 @@ return {
         notifications = true, -- disable notifications
       },
       rooter = {
-        detector = { "lsp", { ".git", "_darcs", ".hg", ".bzr", ".svn", "lua", "Makefile", "package.json" } },
+        detector = { "lsp", { ".git", "_darcs", ".hg", ".bzr", ".svn" }, { "lua", "MarkFile", "packae.json" } },
         ignore = {
           servers = {},
           dirs = {},
